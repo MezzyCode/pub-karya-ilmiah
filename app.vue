3d0410673b8c6a501d79ee2161f5c4022cc93306
leafAlif <leafalifeather@gmail.com>
2023-11-21 21:06:14 +0700

    Prepare Nav Component

--- app.vue
+++ app.vue
@@ -1,36 +1,20 @@
-<script>
-export default {
-  data: () => ({
-    items: [
-      { text: 'Dashboard', icon: 'i-mdi:view-dashboard' },
-      { text: 'Project Anda', icon: 'i-mdi:bookmark-box-multiple' },
-      { text: 'Profil', icon: 'i-mdi:account-box' },
-      { text: 'Download', icon: 'i-mdi:folder-download' },
-      { text: 'Upload', icon: 'i-mdi:folder-upload' },
-    ],
-  }),
-}
+<script setup>
+const route = useRoute();
+const title = computed(() => {
+  return route.meta?.title || route.matched[0].meta?.title || ''
+})
+
+useHead({
+  title,
+  titleTemplate: (t) => (t ? `${t} | ReseaVue` : 'ReseaVue')
+})
 </script>
 
 <template>
-  <div>
-    <v-layout class="rounded rounded-md">
-      <v-app-bar title="Application bar"></v-app-bar>
-
-      <v-navigation-drawer>
-        <v-list>
-          <v-list-item v-for="(item, i) in items" :key="i" :value="item" color="primary">
-            <template v-slot:prepend>
-              <v-icon :icon="item.icon"></v-icon>
-            </template>
-            <v-list-item-title v-text="item.text"></v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-navigation-drawer>
-
-      <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
+  <v-app>
+    <Navigation/>
+      <v-main class="d-flex align-center justify-center">
         <NuxtPage />
       </v-main>
-    </v-layout>
-  </div>
+  </v-app>
 </template>
---
+++ components/Navigation.vue
@@ -0,0 +1,32 @@
+<script setup lang="js">
+const drawer = ref(false)
+const toggleDrawer = () => {
+  drawer.value = !drawer.value;
+  console.log(drawer.value)
+};
+
+const navLinks = [
+  { text: 'Dashboard', icon: 'i-mdi:view-dashboard' },
+  { text: 'Project Anda', icon: 'i-mdi:bookmark-box-multiple' },
+  { text: 'Profil', icon: 'i-mdi:account-box' },
+  { text: 'Download', icon: 'i-mdi:folder-download' },
+  { text: 'Upload', icon: 'i-mdi:folder-upload' },
+]
+</script>
+
+<template>
+  <v-app-bar>
+    <v-app-bar-title>ReseaVue</v-app-bar-title>
+    <v-app-bar-nav-icon icon="i-mdi:menu" @click="toggleDrawer" />
+  </v-app-bar>
+  <v-navigation-drawer v-model="drawer" temporary location="right">
+    <v-list>
+      <v-list-item v-for="(item, i) in navLinks" :key="i" :value="item" color="primary">
+        <template v-slot:prepend>
+          <v-icon :icon="item.icon"></v-icon>
+        </template>
+        <v-list-item-title v-text="item.text"></v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
