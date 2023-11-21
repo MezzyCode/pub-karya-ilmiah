e49594a0e463fb9e084d356094e53376fe93a59d
leafAlif <leafalifeather@gmail.com>
2023-11-22 00:51:56 +0700

    Add some extra nav

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,32 +1,38 @@
 <script setup lang="js">
+const navLinks = [
+  { text: 'Dashboard', href: '/dashboard', icon: 'i-mdi:view-dashboard' },
+  { text: 'Project Anda', href: '/my-projects', icon: 'i-mdi:bookmark-box-multiple' },
+  { text: 'Profil', href: '/profile', icon: 'i-mdi:account-box' },
+  { text: 'Download', href: '/download', icon: 'i-mdi:folder-download' },
+  { text: 'Upload', href: '/upload', icon: 'i-mdi:folder-upload' },
+]
+
 const drawer = ref(false)
 const toggleDrawer = () => {
   drawer.value = !drawer.value;
   console.log(drawer.value)
 };
-
-const navLinks = [
-  { text: 'Dashboard', icon: 'i-mdi:view-dashboard' },
-  { text: 'Project Anda', icon: 'i-mdi:bookmark-box-multiple' },
-  { text: 'Profil', icon: 'i-mdi:account-box' },
-  { text: 'Download', icon: 'i-mdi:folder-download' },
-  { text: 'Upload', icon: 'i-mdi:folder-upload' },
-]
 </script>
 
 <template>
-  <v-app-bar>
-    <v-app-bar-title>ReseaVue</v-app-bar-title>
-    <v-app-bar-nav-icon icon="i-mdi:menu" @click="toggleDrawer" />
+  <v-app-bar density="compact" class="app-bar">
+    <v-app-bar-title><nuxt-link to="/">ReseaVue</nuxt-link></v-app-bar-title>
+    <v-btn to="/" icon="i-mdi:home" rounded="0"/>
+    <v-btn to="/search" icon="i-mdi:search" rounded="0" />
+    <v-app-bar-nav-icon icon="i-mdi:menu" @click="toggleDrawer" rounded="0"/>
   </v-app-bar>
   <v-navigation-drawer v-model="drawer" temporary location="right">
     <v-list>
-      <v-list-item v-for="(item, i) in navLinks" :key="i" :value="item" color="primary">
-        <template v-slot:prepend>
-          <v-icon :icon="item.icon"></v-icon>
-        </template>
-        <v-list-item-title v-text="item.text"></v-list-item-title>
+      <v-list-item v-for="(link, i) in navLinks" :key="i" :value="link" :to="link.href" color="primary">
+        <template v-slot:prepend> <v-icon :icon="link.icon" /> </template>
+        <v-list-item-title v-text="link.text" />
       </v-list-item>
     </v-list>
   </v-navigation-drawer>
 </template>
+
+<style>
+.app-bar {
+  border-bottom: 1px solid var(--v-theme-primary);
+}
+</style>
